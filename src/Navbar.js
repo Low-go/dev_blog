e1a5abe1bf84045965d2996687aaf7db30f7ab9f
Low-go <lorranalvesgaldino98@gmail.com>
2024-07-03 11:09:05 -1000

    add font, space, and image of self to navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,6 +5,10 @@ function Navbar() {
   return (
     <div className='navbar'>
       <p className='main-logo'>Lorran's Dev Blog</p>
+      <div className='me-info'>
+        <p>By,</p>
+        <img src="/images/me.jpg" alt="me" className='me-image'/>
+      </div>
     </div>
   )
 }
