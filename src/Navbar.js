2e7ede7aa4d0209a1f01b6b9eecf3a15da54ca8e
Low-go <lorranalvesgaldino98@gmail.com>
2024-07-02 21:38:54 -1000

    add color to navbar

---
+++ src/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+      <p className='main-logo'>Lorran's Dev Blog</p>
+    </div>
+  )
+}
+
+export default Navbar
