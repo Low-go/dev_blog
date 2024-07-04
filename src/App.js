d9b918b01aa1db757fd3de9a062fe672d5a81053
Low-go <lorranalvesgaldino98@gmail.com>
2024-07-03 23:44:04 -1000

    add white component below header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Navbar from './Navbar';
+import Navbar from './components/Navbar';
+import Filler from './components/Filler';
 
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
 
     <div>
        <Navbar/>
+       <Filler/>
        test
     </div>
 
---
+++ src/components/Filler.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './filler.css';
+
+function Filler() {
+  return (
+    <div className='filler'>
+    </div>
+  )
+}
+
+export default Filler
