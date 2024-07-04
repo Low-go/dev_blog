5ea4e2b8bde83f19c52080f88d3a78e88992656a
Low-go <lorranalvesgaldino98@gmail.com>
2024-07-04 00:02:28 -1000

    move around folders, add some imports, remove some prevoous server side code to make it client side focused

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Filler from './components/Filler';
+import PostComponent from './components/PostComponent';
+import getMarkDownFiles from './utility/markdownParser';
+import { useState, useEffect} from 'react';
 
 
 function App() {
+
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const postsData = await getMarkDownFiles();
+      setPosts(postsData);
+    };
+
+    fetchPosts();
+  }, []);
+
+
   return (
 
     <div>
        <Navbar/>
        <Filler/>
-       test
+       {posts.map((post, index) => (
+        <PostComponent key={index} {...post} />
+      ))}
     </div>
 
 
