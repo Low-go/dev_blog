aadf50b36c2599a068013e2a3b067f74b69e9173
Low-go <lorranalvesgaldino98@gmail.com>
2024-07-10 11:04:32 -1000

    change filenumber.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css'
 
 function App() {
 
-  const fileNumber = 1; // sucks I can't automate this. but incrementing the numnber should be enough
+  const fileNumber = 2; // sucks I can't automate this. but incrementing the numnber should be enough
   const postPaths = Array.from({ length: fileNumber }, (_, index) => `${process.env.PUBLIC_URL}/md_files/blogPost${index}.md`).reverse();
 
   return (
